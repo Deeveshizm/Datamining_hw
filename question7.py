c0620596b40addbe6905744d930f781a7e2f4867
Deevesh Jain <deeveshizm@Deeveshs-MacBook-Pro.local>
2024-01-29 19:46:12 -0500

    First commit

---
+++ question7.py
@@ -0,0 +1,11 @@
+def question7_1():
+    answer = 'increase/decrease'
+    return answer
+
+def question7_2():
+    answer = 'increase/decrease'
+    return answer
+
+def question7_3():
+    answer = [(0, 1), (1, 2), (2, 3), (3, 'infinity')]
+    return answer 
